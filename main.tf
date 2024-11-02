dc6fb3526f1929466f8e3f1f13bdfeb3d20867a1
Marvin Balbutin <balbutinmarvin@gmail.com>
2024-11-02 22:58:39 +0800

    Huge updates.

--- main.tf
+++ main.tf
@@ -12,94 +12,191 @@ provider "aws" {
 }
 
 locals {
-  environment = "dev"
-  projectname = "ecs-cicd-project"
+  Environment = "test"
+  Projectname = "ecs-cicd-infra"
 }
 
 
-module "vpc" {
+###### APPLICATION VPC ######
+
+module "module_app_vpc" {
   source  = "app.terraform.io/marvsmpb/vpc-module-marvs/aws"
   version = "1.0.3"
 
-  vpc_cidr_block = "10.0.0.0/16"
+  vpc_cidr_block = "10.100.0.0/16"
   vpc_tags = {
-    Name        = "${local.projectname}-${local.environment}-vpc"
-    environment = "${local.environment}"
+    Name        = "${local.Projectname}-${local.Environment}-app-vpc"
+    Environment = "${local.Environment}"
   }
 }
 
 
-module "nlb_subnet1" {
+module "module_app_subnet_nlb1" {
   source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
-  version = "0.0.2"
+  version = "0.0.14"
 
   subnet_az   = "ap-southeast-1a"
-  subnet_cidr = "10.0.10.0/24"
-  subnet_vpc  = module.vpc.output_vpc_id
+  subnet_cidr = "10.100.10.0/24"
+  subnet_vpc  = module.module_app_vpc.output_vpc_id
   subnet_tags = {
-    Name = "${local.projectname}-${local.environment}-nlb-subnet1"
+    Name        = "${local.Projectname}-${local.Environment}-app-subnet-nlb1"
+    Environment = "${local.Environment}"
   }
 }
 
-module "nlb_subnet2" {
+module "module_app_subnet_nlb2" {
   source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
-  version = "0.0.2"
+  version = "0.0.14"
 
   subnet_az   = "ap-southeast-1b"
-  subnet_cidr = "10.0.20.0/24"
-  subnet_vpc  = module.vpc.output_vpc_id
+  subnet_cidr = "10.100.20.0/24"
+  subnet_vpc  = module.module_app_vpc.output_vpc_id
   subnet_tags = {
-    Name = "${local.projectname}-${local.environment}-nlb-subnet2"
+    Name        = "${local.Projectname}-${local.Environment}-app-subnet-nlb2"
+    Environment = "${local.Environment}"
   }
 }
 
-module "app_subnet1" {
+module "module_app_subnet1" {
   source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
-  version = "0.0.2"
+  version = "0.0.14"
 
   subnet_az   = "ap-southeast-1a"
-  subnet_cidr = "10.0.100.0/24"
-  subnet_vpc  = module.vpc.output_vpc_id
+  subnet_cidr = "10.100.100.0/24"
+  subnet_vpc  = module.module_app_vpc.output_vpc_id
   subnet_tags = {
-    Name = "${local.projectname}-${local.environment}-app-subnet1"
+    Name        = "${local.Projectname}-${local.Environment}-app-subnet-app1"
+    Environment = "${local.Environment}"
   }
 }
 
-module "app_subnet2" {
+module "module_app_subnet2" {
   source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
-  version = "0.0.2"
+  version = "0.0.14"
 
   subnet_az   = "ap-southeast-1b"
-  subnet_cidr = "10.0.200.0/24"
-  subnet_vpc  = module.vpc.output_vpc_id
+  subnet_cidr = "10.100.200.0/24"
+  subnet_vpc  = module.module_app_vpc.output_vpc_id
   subnet_tags = {
-    Name = "${local.projectname}-${local.environment}-app-subnet2"
+    Name        = "${local.Projectname}-${local.Environment}-app-subnet-app1"
+    Environment = "${local.Environment}"
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = module.vpc.output_vpc_id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_internet_gateway_attachment" "example" {
-  internet_gateway_id = aws_internet_gateway.igw.id
-  vpc_id              = module.vpc.output_vpc_id
-}
-
-module "rtb-app" {
-  source  = "app.terraform.io/marvsmpb/rtb-marvs/aws"
+module "module_app_vpc_peering" {
+  source  = "app.terraform.io/marvsmpb/vpc-peering-owner-marvs/aws"
   version = "0.0.3"
 
-  rtb_vpc = module.vpc.output_vpc_id
+  vpc_id      = module.vpc_infra_app.output_vpc_id
+  peer_vpc_id = module.module_network_vpc_peering.output_peering_id
+  owner_tags = {
+    Name        = "${local.Projectname}-${local.Environment}-app-peering"
+    Environment = "${local.Environment}"
+  }
+}
+
+module "module_rtb_app" {
+  source  = "app.terraform.io/marvsmpb/rtb-marvs/aws"
+  version = "0.0.6"
+
+  rtb_vpc = module.module_app_vpc.output_vpc_id
+
+  route_peering_bool           = true
+  route_peering                = module.module_app_vpc_peering.output_peering_id
   route_destination_cidr_block = "0.0.0.0/0"
+
+
+
   rtb_tags = {
-    Name = "${local.projectname}-${local.environment}-app-rtb"
+    Name        = "${local.Projectname}-${local.Environment}-app-rtb-app1"
+    Environment = "${local.Environment}"
+  }
+}
+
+
+
+
+
+
+
+
+
+###### NETWORK VPC ######
+
+module "module_network_vpc" {
+  source  = "app.terraform.io/marvsmpb/vpc-module-marvs/aws"
+  version = "1.0.3"
+
+  vpc_cidr_block = "10.200.0.0/16"
+  vpc_tags = {
+    Name        = "${local.Projectname}-${local.Environment}-network-vpc"
+    Environment = "${local.Environment}"
+  }
+}
+
+
+module "module_network_subnet_pub1" {
+  source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
+  version = "0.0.14"
+
+  subnet_vpc  = module.module_network_vpc.output_vpc_id
+  subnet_az   = "ap-southeast-1a"
+  subnet_cidr = "10.200.10.0/24"
+  subnet_tags = {
+    Name        = "${local.Projectname}-${local.Environment}-network-subnet_pub1"
+    Environment = "${local.Environment}"
   }
 
-  route_internet_gateway_bool = true
-  route_internet_gateway      = aws_internet_gateway.igw.id
+  subnet_public_bool = true
+  igw_tags = {
+    Name        = "${local.Projectname}-${local.Environment}-network-subnet_pub_igw"
+    Environment = "${local.Environment}"
+  }
 }
+
+module "module_network_subnet_pub2" {
+  source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
+  version = "0.0.14"
+
+  subnet_vpc  = module.module_network_vpc.output_vpc_id
+  subnet_az   = "ap-southeast-1b"
+  subnet_cidr = "10.200.20.0/24"
+  subnet_tags = {
+    Name        = "${local.Projectname}-${local.Environment}-network-subnet_pub2"
+    Environment = "${local.Environment}"
+  }
+}
+
+module "module_network_vpc_peering" {
+  source  = "app.terraform.io/marvsmpb/vpc-peering-owner-marvs/aws"
+  version = "0.0.3"
+
+  vpc_id      = module.module_network_vpc.output_vpc_id
+  peer_vpc_id = module.module_app_vpc_peering.output_peering_id
+  owner_tags = {
+    Name        = "${local.Projectname}-${local.Environment}-network-peering"
+    Environment = "${local.Environment}"
+  }
+}
+
+module "module_network_rtb" {
+  source  = "app.terraform.io/marvsmpb/rtb-marvs/aws"
+  version = "0.0.6"
+
+  rtb_vpc = module.module_network_vpc.output_vpc_id
+
+  route_peering_bool           = true
+  route_peering                = module.module_app_vpc_peering.output_peering_id
+  route_destination_cidr_block = "10.100.0.0/16"
+
+  route_internet_gateway_bool                   = true
+  route_internet_gateway                        = module.module_network_subnet_pub1.outputs_route_route_internet_gateway
+  route_internet_gateway_destination_cidr_block = "0.0.0.0/0"
+
+  rtb_tags = {
+    Name        = "${local.Projectname}-${local.Environment}-network-rtb-app1"
+    Environment = "${local.Environment}"
+  }
+}
+
+
