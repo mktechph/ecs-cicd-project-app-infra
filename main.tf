0403d67f7538c0300fb9559fb5d8ef07ccb56a9d
Marvin Balbutin <balbutinmarvin@gmail.com>
2024-11-03 23:16:35 +0800

    Update module version to fix issue.

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ resource "aws_route_table_association" "rtb_app_assoc_nlb_subnet2" {
 
 module "module_app_ecs_ecr_subnet1_endpoint" {
   source  = "app.terraform.io/marvsmpb/vpc-endpoint-ecs-ecr-marvs/aws"
-  version = "0.0.5"
+  version = "0.0.6"
 
   vpc_id = module.module_app_vpc.output_vpc_id
 
@@ -150,7 +150,7 @@ module "module_app_ecs_ecr_subnet1_endpoint" {
 
 module "module_app_ecs_ecr_subnet2_endpoint" {
   source  = "app.terraform.io/marvsmpb/vpc-endpoint-ecs-ecr-marvs/aws"
-  version = "0.0.5"
+  version = "0.0.6"
 
   vpc_id = module.module_app_vpc.output_vpc_id
 
@@ -256,3 +256,4 @@ resource "aws_route_table_association" "rtb_network_assoc_public_subnet2" {
   subnet_id      = module.module_network_pub_subnet2.outputs_subnet_id
   route_table_id = module.module_network_rtb.outputs_rtb_id
 }
+
