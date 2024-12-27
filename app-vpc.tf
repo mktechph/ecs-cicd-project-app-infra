fb53093db6a02393d275307f708cc68525950596
mktechph <114472493+mktechph@users.noreply.github.com>
2024-12-27 14:42:23 +0800

    Fix duplicate.

--- app-vpc.tf
+++ app-vpc.tf
@@ -135,41 +135,7 @@ resource "aws_route_table_association" "rtb_app_assoc_nlb_subnet2" {
   route_table_id = module.module_rtb_app.outputs_rtb_id
 }
 
-module "module_app_ecs_ecr_subnet1_endpoint" {
-  source  = "app.terraform.io/marvsmpb/vpc-endpoint-ecs-ecr-marvs/aws"
-  version = "0.0.6"
 
-  vpc_id = module.module_app_vpc.output_vpc_id
-
-  ecs_endpoint_subnet_id           = [module.module_app_subnet1.outputs_subnet_id]
-  ecs_agent_endpoint_subnet_id     = [module.module_app_subnet1.outputs_subnet_id]
-  ecs_telemetry_endpoint_subnet_id = [module.module_app_subnet1.outputs_subnet_id]
-  ecr_api_endpoint_subnet_id       = [module.module_app_subnet1.outputs_subnet_id]
-  ecr_dkr_endpoint_subnet_id       = [module.module_app_subnet1.outputs_subnet_id]
-
-  endpoint_tags = {
-    Name        = "${local.Projectname}-${local.Environment}-app-ecs-ecr-endpoint-subnet1-app1"
-    Environment = "${local.Environment}"
-  }
-}
-
-module "module_app_ecs_ecr_subnet2_endpoint" {
-  source  = "app.terraform.io/marvsmpb/vpc-endpoint-ecs-ecr-marvs/aws"
-  version = "0.0.6"
-
-  vpc_id = module.module_app_vpc.output_vpc_id
-
-  ecs_endpoint_subnet_id           = [module.module_app_subnet2.outputs_subnet_id]
-  ecs_agent_endpoint_subnet_id     = [module.module_app_subnet2.outputs_subnet_id]
-  ecs_telemetry_endpoint_subnet_id = [module.module_app_subnet2.outputs_subnet_id]
-  ecr_api_endpoint_subnet_id       = [module.module_app_subnet2.outputs_subnet_id]
-  ecr_dkr_endpoint_subnet_id       = [module.module_app_subnet2.outputs_subnet_id]
-
-  endpoint_tags = {
-    Name        = "${local.Projectname}-${local.Environment}-app-ecs-ecr-endpoint-subnet2-app1"
-    Environment = "${local.Environment}"
-  }
-}
 
 
 
