97e40f941b7fd30a2bac0b3209e1609219c08bbe
mktechph <114472493+mktechph@users.noreply.github.com>
2024-12-31 16:45:17 +0800

    remove network vpc and tgw temporarily.

--- app-vpc.tf
+++ app-vpc.tf
@@ -11,31 +11,31 @@ module "module_app_vpc" {
   }
 }
 
-module "module_app_tgw_subnet1" {
-  source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
-  version = "0.0.14"
+#module "module_app_tgw_subnet1" {
+#  source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
+#  version = "0.0.14"
+#
+#  subnet_vpc  = module.module_app_vpc.output_vpc_id
+#  subnet_az   = "ap-southeast-1a"
+#  subnet_cidr = "10.100.10.0/24"
+#  subnet_tags = {
+#    Name        = "${local.Projectname}-${local.Environment}-app-tgw-subnet1"
+#    Environment = "${local.Environment}"
+#  }
+#}
 
-  subnet_vpc  = module.module_app_vpc.output_vpc_id
-  subnet_az   = "ap-southeast-1a"
-  subnet_cidr = "10.100.10.0/24"
-  subnet_tags = {
-    Name        = "${local.Projectname}-${local.Environment}-app-tgw-subnet1"
-    Environment = "${local.Environment}"
-  }
-}
-
-module "module_app_tgw_subnet2" {
-  source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
-  version = "0.0.14"
-
-  subnet_vpc  = module.module_app_vpc.output_vpc_id
-  subnet_az   = "ap-southeast-1b"
-  subnet_cidr = "10.100.20.0/24"
-  subnet_tags = {
-    Name        = "${local.Projectname}-${local.Environment}-app-tgw-subnet2"
-    Environment = "${local.Environment}"
-  }
-}
+#module "module_app_tgw_subnet2" {
+#  source  = "app.terraform.io/marvsmpb/subnet-marvs/aws"
+#  version = "0.0.14"
+#
+#  subnet_vpc  = module.module_app_vpc.output_vpc_id
+#  subnet_az   = "ap-southeast-1b"
+#  subnet_cidr = "10.100.20.0/24"
+#  subnet_tags = {
+#    Name        = "${local.Projectname}-${local.Environment}-app-tgw-subnet2"
+#    Environment = "${local.Environment}"
+#  }
+#}
 
 
 module "module_app_subnet_nlb1" {
@@ -107,21 +107,21 @@ module "module_rtb_app" {
   }
 }
 
-# TGW ROUTE
-resource "aws_route" "route_app_subnet_to_tgw" {
-  route_table_id         = module.module_rtb_app.outputs_rtb_id
-  destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
-}
+## TGW ROUTE
+#resource "aws_route" "route_app_subnet_to_tgw" {
+#  route_table_id         = module.module_rtb_app.outputs_rtb_id
+#  destination_cidr_block = "0.0.0.0/0"
+#  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+#}
 
-resource "aws_route_table_association" "rtb_app_assoc_tgw_subnet1" {
-  subnet_id      = module.module_app_tgw_subnet1.outputs_subnet_id
-  route_table_id = module.module_rtb_app.outputs_rtb_id
-}
-resource "aws_route_table_association" "rtb_app_assoc_tgw_subnet2" {
-  subnet_id      = module.module_app_tgw_subnet2.outputs_subnet_id
-  route_table_id = module.module_rtb_app.outputs_rtb_id
-}
+#resource "aws_route_table_association" "rtb_app_assoc_tgw_subnet1" {
+#  subnet_id      = module.module_app_tgw_subnet1.outputs_subnet_id
+#  route_table_id = module.module_rtb_app.outputs_rtb_id
+#}
+#resource "aws_route_table_association" "rtb_app_assoc_tgw_subnet2" {
+#  subnet_id      = module.module_app_tgw_subnet2.outputs_subnet_id
+#  route_table_id = module.module_rtb_app.outputs_rtb_id
+#}
 resource "aws_route_table_association" "rtb_app_assoc_app_subnet1" {
   subnet_id      = module.module_app_subnet1.outputs_subnet_id
   route_table_id = module.module_rtb_app.outputs_rtb_id
