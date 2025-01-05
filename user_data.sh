d31f30d73f97e451654cd6e0ae090b0d34fa40bc
mktechph <114472493+mktechph@users.noreply.github.com>
2025-01-05 13:35:53 +0800

    Add ecs cluster in user data

--- user_data.sh
+++ user_data.sh
@@ -3,3 +3,4 @@ cd /tmp
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl enable amazon-ssm-agent
 sudo systemctl start amazon-ssm-agent
+echo ECS_CLUSTER=ecs-cicd-fe-oauth >> /etc/ecs/ecs.config
