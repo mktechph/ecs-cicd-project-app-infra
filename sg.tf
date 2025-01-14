0421081f2f325740e2812e1001bd55a8d63a429e
mktechph <114472493+mktechph@users.noreply.github.com>
2025-01-15 00:49:07 +0800

    Update sg

--- sg.tf
+++ sg.tf
@@ -7,7 +7,8 @@ resource "aws_security_group" "sg_alb" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.100.30.101/32", "10.100.40.101/32"]
+    cidr_blocks = ["0.0.0.0/0", "0.0.0.0/0"]
+    #cidr_blocks = ["10.100.30.101/32", "10.100.40.101/32"]
     #security_groups = [aws_security_group.sg_nlb.id]
   }
 
